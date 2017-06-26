9b45e897541228df1b503685bb08635dd31a9e01
Nahum Maciel <nahumamaciel@gmail.com>
2017-06-26 16:10:53 -0700

    Began to work on html templates using jinja.

---
+++ format.py
@@ -0,0 +1,15 @@
+markup = """
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>{0}</title>
+        </head>
+        <body>
+            <h1>{1}</h1>
+        </body>
+    </html>
+"""
+
+markup = markup.format('My page', 'page Heading')
+
+print(markup)
--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 from flask import Flask, request
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env =jinja2.Environment(loader = jinja2.FileSystemLoader (template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """<!DOCTYPE html>
-    <html>
-        <body>
-            <form action="/hello" method="post">
-                <label for="first-name">First Name:</label>
-                <input id="first-name" type="text" name="first-name" />
-                <input type="submit" />
-            </form>
-        </body>
-    </html>
-"""
-
 @app.route("/")
 def index():
-    return form
+    template = jinja_env.get_template('hello_form.html')
+    return template.render()
 
 @app.route("/hello", methods=['post'])
 def hello():
     first_name = request.form["first-name"]
-    return "<h1>Hello, " + first_name + "!</h1>"
+    template = jinja_env.get_template('hello_greeting.html')
+    return template.render(name=first_name)
 
 app.run()
